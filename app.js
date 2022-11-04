da111e1c79609189270e11171887a19779d32746
Max Guclu <max.guclu@chasacademy.se>
2022-11-05 00:15:36 +0100

    Changed alot of the structure. Rather than hardcoded hidden list elements in HTML changed to object with array and appended lists with guessed values. Moved two functions from within click function, to separate functions and called on them at click instead. removed load event to hide modal to hiding it at var declaration

--- app.js
+++ app.js
@@ -2,22 +2,35 @@
 const welcomeBox = 
 document.querySelector('.welcome')
 
-//play game
+//play game button
 const gameBtn =
 document.querySelector(".playGame")
 
-//Remember append OL to gamemodal with lists of guessess
+//game modal window
 const gameModal = 
 document.querySelector('.modalGame')
+gameModal.classList.add('hide')
 
 //Guess button 
 const guessBtn = 
 document.querySelector('.enterGuess')
 
-//when this is pressed, value from input to variable guessInput
+//when this is pressed, value from input to variable guessInput in int
 let guessInput = guessBtn.addEventListener('click',function(){
     guessInput = parseInt(document.querySelector('.guessField').value)
 })
+
+//Ordered list
+const guessList = 
+document.querySelector('.guessList')
+
+//object for guesses
+let guess = {
+    guessed: [null, null, null, null, null]
+}
+
+//arraycounter
+let i = 0
 
 //H3 that gives tips
 const cta = 
@@ -26,145 +39,60 @@ document.querySelector('.enterNo')
 //a number between 1-100
 let rng = null 
 
-
-//Ordered list
-const guessList = 
-document.querySelector('.guessList')
-
-let guessOne =
-document.querySelector(".guessOne")
-let guessTwo =
-document.querySelector(".guessTwo")
-let guessThree =
-document.querySelector(".guessThree")
-let guessFour =
-document.querySelector(".guessFour")
-let guessFive =
-document.querySelector(".guessFive")
-
-let tooHigh = "(Too High!)"
-let tooLow = "(Too Low!)"
-
-
-//arraycounter
-let x = 0
-
 //function to randomize a number between 1-100
 function rngNumber() { 
-    rng = Math.floor((Math.random() * 100) + 1)
+   rng = Math.floor((Math.random() * 100) + 1)
 }
 
-//when page loads, hide the modal
-window.addEventListener('load', (event) => {
-    gameModal.classList.add('hide')
-});
-
-//When play game button is pressed, hide background, show modal
+//When play game button is pressed, hide background, show modal, generate number
 gameBtn.addEventListener('click',function(){
     welcomeBox.classList.add('hide')
     gameModal.classList.remove('hide')
-    guessOne.classList.add('hide')
-    guessTwo.classList.add('hide')
-    guessThree.classList.add('hide')
-    guessFour.classList.add('hide')
-    guessFive.classList.add('hide')
     rngNumber()
 })
 
-
+//Check for button click, run the functions if within span
 guessBtn.addEventListener('click',function(){
     if (guessInput > 0 && guessInput < 101) {
-        if (guessInput == rng) {
-            cta.innerHTML = `<br> ${guessInput} is correct!`
-        }
-        else if (guessInput > rng) {
-            cta.innerHTML = `<br> ${guessInput} is too high, guess lower!`
-
-        //add guess to an array or list
-        }   
-        else if (guessInput < rng) {
-            cta.innerHTML = `<br> ${guessInput} is too low, guess higher!`
-
-        //add guess to an array or list
-        }
-    }
-    else {
+        checkNumber()
+        guessedList()
+    } else {
         cta.innerHTML = `<br> ${guessInput} is not within 1-100`
     }
-
-if (guessInput > 0 && guessInput < 101 ) {
-    if( x > 4 ) {
-        cta.innerHTML = `<br> You've guessed too many times!`
-}
-
-if (x == 4){
-    guessFive.classList.remove('hide')
-    if (guessInput < rng) {
-        guessFive.innerHTML = guessInput + tooLow
-    }
-    if (guessInput > rng) {
-        guessFive.innerHTML = guessInput + tooHigh
-    }
-    x++
-}
-
-if (x == 3){
-    guessFour.classList.remove('hide')
-    if (guessInput < rng) {
-        guessFour.innerHTML = guessInput + tooLow
-    }
-    if (guessInput > rng) {
-        guessFour.innerHTML = guessInput + tooHigh
-    }
-    x++
-}
-
-    if (x == 2){
-        guessThree.classList.remove('hide')
-        if (guessInput < rng) {
-            guessThree.innerHTML = guessInput + tooLow
-        }
-        if (guessInput > rng) {
-            guessThree.innerHTML = guessInput + tooHigh
-        }
-        x++
-    }
-
-    if (x == 1){
-        guessTwo.classList.remove('hide')
-        if (guessInput < rng) {
-            guessTwo.innerHTML = guessInput + tooLow
-        }
-        if (guessInput > rng) {
-            guessTwo.innerHTML = guessInput + tooHigh
-        }
-        x++
-    }
-
-    if (x == 0){
-    guessOne.classList.remove('hide')
-    if (guessInput < rng) {
-        guessOne.innerHTML = guessInput + tooLow
-    }
-    if (guessInput > rng) {
-        guessOne.innerHTML = guessInput + tooHigh
-    }
-    x++
-    }
-}
-
 })
 
-/*     prevGuess.push(guessInput)
-    li.textContent = `${prevGuess[x]}`
-    guessList.appendChild(li)
-    x++ */
+//function to check if guessed number is equal, below or above rng.
+function checkNumber() {
+    if (guessInput == rng) {
+        cta.innerHTML = `<br> ${guessInput} is correct!`
+    }
+    else if (guessInput > rng) {
+        cta.innerHTML = `<br> ${guessInput} is too high, guess lower!`
+    } else if (guessInput < rng) {
+        cta.innerHTML = `<br> ${guessInput} is too low, guess higher!`
+    } 
+}
 
-    
-//array for guesses
-/* let prevGuess = [guessOne, guessTwo, guessThree] */
 
-
-//new list item
-/* const li = 
-document.createElement('li') */
+//function to log guesses up to five.
+function guessedList() {
+    let tooHigh = "(Too High!)"
+    let tooLow = "(Too Low!)"
+    if(i<=4) { 
+    console.log('i: ' + i)
+    guess.guessed[i] = guessInput
+    console.log(guess.guessed[i])
+    let listElement = document.createElement("li")
+    if (guessInput == rng) {
+        } else if (guessInput < rng) {
+    listElement.innerHTML = guessInput + tooLow
+    guessList.appendChild(listElement)
+    } else if (guessInput > rng) {
+        listElement.innerHTML = guessInput + tooHigh
+        guessList.appendChild(listElement)
+        }
+    } else {
+        cta.innerHTML = `<br> You've guessed too many times!`
+    }
+    i++
+}
