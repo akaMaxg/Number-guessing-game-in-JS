255ac92dc6651b51ecc08acd26e1719341aeb601
Max Guclu <max.guclu@chasacademy.se>
2022-11-04 19:57:18 +0100

    Guessing game. Added a list to HTML for guesses Hiding list with js At click of button event - value is compared to rng if not withing 1-100 error message will display and no log else the function will compare than input guess to list

--- app.js
+++ app.js
@@ -1,54 +1,170 @@
+//start page
 const welcomeBox = 
-document.querySelector('.welcome');
+document.querySelector('.welcome')
+
+//play game
 const gameBtn =
-document.querySelector(".playGame");
+document.querySelector(".playGame")
 
 //Remember append OL to gamemodal with lists of guessess
-//add an H2 element after h1 with clues
 const gameModal = 
-document.querySelector('.modalGame');
+document.querySelector('.modalGame')
 
-//Get and store values from this input
-const guessInput = 
-document.querySelector('.guessField').value;
-
-//when this is pressed, sends value of guessInput compared to a rng
+//Guess button 
 const guessBtn = 
 document.querySelector('.enterGuess')
 
-let rng = null; 
+//when this is pressed, value from input to variable guessInput
+let guessInput = guessBtn.addEventListener('click',function(){
+    guessInput = parseInt(document.querySelector('.guessField').value)
+})
 
+//H3 that gives tips
+const cta = 
+document.querySelector('.enterNo')
+
+//a number between 1-100
+let rng = null 
+
+
+//Ordered list
+const guessList = 
+document.querySelector('.guessList')
+
+let guessOne =
+document.querySelector(".guessOne")
+let guessTwo =
+document.querySelector(".guessTwo")
+let guessThree =
+document.querySelector(".guessThree")
+let guessFour =
+document.querySelector(".guessFour")
+let guessFive =
+document.querySelector(".guessFive")
+
+let tooHigh = "(Too High!)"
+let tooLow = "(Too Low!)"
+
+
+//arraycounter
+let x = 0
+
+//function to randomize a number between 1-100
 function rngNumber() { 
-    rng = Math.floor((Math.random() * 100) + 1);
+    rng = Math.floor((Math.random() * 100) + 1)
 }
 
-
+//when page loads, hide the modal
 window.addEventListener('load', (event) => {
     gameModal.classList.add('hide')
 });
 
+//When play game button is pressed, hide background, show modal
 gameBtn.addEventListener('click',function(){
     welcomeBox.classList.add('hide')
     gameModal.classList.remove('hide')
+    guessOne.classList.add('hide')
+    guessTwo.classList.add('hide')
+    guessThree.classList.add('hide')
+    guessFour.classList.add('hide')
+    guessFive.classList.add('hide')
+    rngNumber()
 })
 
 
 guessBtn.addEventListener('click',function(){
-    rngNumber()
-    if (parseInt(guessInput) > 0 && parseInt(guessInput) < 101) {
-        if (parse.int(guessInput) == rng) {
-        //Add HTML that says it was correct
+    if (guessInput > 0 && guessInput < 101) {
+        if (guessInput == rng) {
+            cta.innerHTML = `<br> ${guessInput} is correct!`
         }
-        else if (parseInt(guessInput) > rng) {
-        //Add HTML says you need to guess lower
+        else if (guessInput > rng) {
+            cta.innerHTML = `<br> ${guessInput} is too high, guess lower!`
+
         //add guess to an array or list
         }   
-        else if (parseInt(guessInput) < rng) {
-        //add HTML that says guess higher
+        else if (guessInput < rng) {
+            cta.innerHTML = `<br> ${guessInput} is too low, guess higher!`
+
         //add guess to an array or list
         }
     }
     else {
-        console.log('Guess a number  between 1-100')
+        cta.innerHTML = `<br> ${guessInput} is not within 1-100`
     }
+
+if (guessInput > 0 && guessInput < 101 ) {
+    if( x > 4 ) {
+        cta.innerHTML = `<br> You've guessed too many times!`
+}
+
+if (x == 4){
+    guessFive.classList.remove('hide')
+    if (guessInput < rng) {
+        guessFive.innerHTML = guessInput + tooLow
+    }
+    if (guessInput > rng) {
+        guessFive.innerHTML = guessInput + tooHigh
+    }
+    x++
+}
+
+if (x == 3){
+    guessFour.classList.remove('hide')
+    if (guessInput < rng) {
+        guessFour.innerHTML = guessInput + tooLow
+    }
+    if (guessInput > rng) {
+        guessFour.innerHTML = guessInput + tooHigh
+    }
+    x++
+}
+
+    if (x == 2){
+        guessThree.classList.remove('hide')
+        if (guessInput < rng) {
+            guessThree.innerHTML = guessInput + tooLow
+        }
+        if (guessInput > rng) {
+            guessThree.innerHTML = guessInput + tooHigh
+        }
+        x++
+    }
+
+    if (x == 1){
+        guessTwo.classList.remove('hide')
+        if (guessInput < rng) {
+            guessTwo.innerHTML = guessInput + tooLow
+        }
+        if (guessInput > rng) {
+            guessTwo.innerHTML = guessInput + tooHigh
+        }
+        x++
+    }
+
+    if (x == 0){
+    guessOne.classList.remove('hide')
+    if (guessInput < rng) {
+        guessOne.innerHTML = guessInput + tooLow
+    }
+    if (guessInput > rng) {
+        guessOne.innerHTML = guessInput + tooHigh
+    }
+    x++
+    }
+}
+
 })
+
+/*     prevGuess.push(guessInput)
+    li.textContent = `${prevGuess[x]}`
+    guessList.appendChild(li)
+    x++ */
+
+    
+//array for guesses
+/* let prevGuess = [guessOne, guessTwo, guessThree] */
+
+
+//new list item
+/* const li = 
+document.createElement('li') */
